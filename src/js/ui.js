9dacfeeecc234e6c594042150d772692dcfeff67
rhe24 <rhe24@aber.ac.uk>
2018-11-09 20:05:38 +0000

    Added ui module

---
+++ src/js/ui.js
@@ -0,0 +1,24 @@
+/**
+  * ui.js
+  * Handle all of the game's UI components
+  *
+  * @author Rhys Evans (rhe24@aber.ac.uk)
+  * @version 9/11/2018
+*/
+
+/**
+  * Create ui module
+*/
+var Ui = (function(){
+
+  'use strict';
+
+  /* ===== PRIVATE METHODS ===== */
+
+  /* ===== PUBLIC METHODS ===== */
+
+  /* ===== EXPORT PUBLIC METHODS ===== */
+  return{
+  };
+
+}());
